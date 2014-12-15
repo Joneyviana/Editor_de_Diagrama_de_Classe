48a44c757cfff061bf4bd823b239bdceaf5d78f5
Joneyviana <arcistiel@gmail.com>
2014-12-15 07:33:59 -0200

    nova tela

---
+++ src/editor/editors/Tela.java
@@ -0,0 +1,16 @@
+package editor.editors;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Tela extends Composite{
+
+	private retangulo inicio_associacao;
+	private retangulo fim_associacao;
+	public Tela(Composite parent, int style) {
+		super(parent, style);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+}
