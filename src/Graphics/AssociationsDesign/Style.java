72999ff86030844acc1990cab28c670f2eab2a70
Joneyviana <arcistiel@gmail.com>
2015-12-11 08:10:06 -0200

    modificando a indentificação de associação no retangulo

--- src/Graphics/AssociationsDesign/Style.java
+++ src/Graphics/AssociationsDesign/Style.java
@@ -15,11 +15,11 @@ public abstract void addfeature(GC gc ,Linha l) ;
 
 public retangulo verfificaretangulo(PageDiagrams p,Ponto ponto){
 	for(retangulo ret :p.rets){
-		
-		if(Linha.ispontodentrodequadrado(ponto, ret.x, ret.y, ret.width, ret.height)){
+		if (ret !=null){
+		if(Linha.ispontodentrodequadrado(ponto, ret.getLocation().x, ret.getLocation().y, ret.getSize().x, ret.getSize().y)){
 			
 			return ret;
-		}
+		}}
 	}
   return null ;
 }
